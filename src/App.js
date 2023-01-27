f76c2aeefa0e370aa8b3a15ddf311119f8fe4e8a
Anne-Sophie SIMON <annesophiesimon@users.noreply.github.com>
2023-01-27 18:12:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React Test
+          React Test Anne
         </p>
         <a
           className="App-link"
